afc4149ff4d103ba52319312f1bcc9340914b995
Manika007 <manikadwivedi007@gmail.com>
2024-09-28 23:13:01 +0530

    completed Navbar

--- Frontend/src/components/Navbar/Navbar.jsx
+++ Frontend/src/components/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import React,{useState} from 'react'
 import "./Navbar.scss"
 function Navbar() {
+  const [open,setOpen]= useState(false)
   return (
     <nav>
         <div className="left">
@@ -18,7 +19,16 @@ function Navbar() {
         <a href='#'>Sign in</a>
         <a href='#' className='register'>Sign up</a>
         <div className="menuIcon">
-          <img src='/' alt=""/>
+          <img src='/menu.png' alt="" onClick={()=> setOpen(prev=>!prev)}/>
+        </div>
+        <div className={open ? "menu active" : "menu"}>
+            <a href='#'>Home</a>
+            <a href='#'>About</a>
+            <a href='#'>Contact</a>
+            <a href='#'>Agents</a>
+            <a href='#'>Sign in</a>
+            <a href='#'>Sign up</a>
+
         </div>
         </div>
     </nav>
